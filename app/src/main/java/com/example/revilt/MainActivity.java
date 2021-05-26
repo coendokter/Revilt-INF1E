544ceba8f04bc102677677548cb246f8ba566b9c
coendokter97 <coendokter97@outlook.com>
2021-05-26 12:41:07 +0200

    New project
    .

--- app/src/main/java/com/example/revilt/MainActivity.java
+++ app/src/main/java/com/example/revilt/MainActivity.java
@@ -1,47 +1,13 @@
 package com.example.revilt;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import java.util.HashMap;
-import kotlin.Metadata;
-import org.jetbrains.annotations.Nullable;
+import android.os.Bundle;
 
-@Metadata(
-        mv = {1, 1, 15},
-        bv = {1, 0, 3},
-        k = 1,
-        d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005¢\u0006\u0002\u0010\u0002J\u0012\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u0014¨\u0006\u0007"},
-        d2 = {"Lcom/example/revilt/MainActivity;", "Landroid/support/v7/app/AppCompatActivity;", "()V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"}
-)
-public final class MainActivity extends AppCompatActivity {
-    private HashMap _$_findViewCache;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Make sure this is before calling super.onCreate
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-    }
-
-    public View _$_findCachedViewById(int var1) {
-        if (this._$_findViewCache == null) {
-            this._$_findViewCache = new HashMap();
-        }
-
-        View var2 = (View)this._$_findViewCache.get(var1);
-        if (var2 == null) {
-            var2 = this.findViewById(var1);
-            this._$_findViewCache.put(var1, var2);
-        }
-
-        return var2;
-    }
-
-    public void _$_clearFindViewByIdCache() {
-        if (this._$_findViewCache != null) {
-            this._$_findViewCache.clear();
-        }
+        setContentView(R.layout.activity_main);
     }
 }
-
