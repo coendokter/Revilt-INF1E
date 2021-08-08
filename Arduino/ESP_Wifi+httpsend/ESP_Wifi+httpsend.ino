603d1297cbdc425ee55b91201660b84bc851ca79
EJZandberg <73105419+EJZandberg@users.noreply.github.com>
2021-08-08 11:27:03 +0200

    code conventies

--- Arduino/ESP_Wifi+httpsend/ESP_Wifi+httpsend.ino
+++ Arduino/ESP_Wifi+httpsend/ESP_Wifi+httpsend.ino
@@ -23,28 +23,34 @@ const int LOADCELL_SCK_PIN = 26;
 boolean isSetup;
 int calibration = 0;
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);                     // initiate Serial monitor
   EEPROM.begin(EEPROM_SIZE);                // Starts library to safe bytes
-  if (EEPROM.read(0) == 255 || EEPROM.read(0) == 0){               // if the value on spot 0 is equal to 255, we reset it to the value we need, 1
+  if (EEPROM.read(0) == 255 || EEPROM.read(0) == 0)
+  {              
     EEPROM.write(0, 1);
   }
   AsyncWiFiManager wifiManager(&server,&dns);
-  if(EEPROM.read(0) == 1){                  // If the first byte of the EEPROM library is equal to 1, we will execute the following code
-    wifiManager.startConfigPortal(ViltName);// Starts configportal no matter if it connected to an existing wifi
-    if(millis() == 20000){                  // Gives you 60 seconds to change wifi networks if you desire
+  if(EEPROM.read(0) == 1) // If the first byte of the EEPROM library is equal to 1, we will execute the following code
+  {                  
+    wifiManager.startConfigPortal(ViltName); // Starts configportal no matter if it connected to an existing wifi
+    if(millis() == 20000) // Gives you 60 seconds to change wifi networks if you desire
+	{                  
       EEPROM.write(0, 2);                     // Changes the 0 byte from 1 to 2
       EEPROM.commit();                        // Commits the change in the byte
       ESP.restart();
     }
   }
-  else if(EEPROM.read(0) == 2){            //if the 0 byte in the EEPROM library is equal to 2, we will execute the following code
+  else if(EEPROM.read(0) == 2) //if the 0 byte in the EEPROM library is equal to 2, we will execute the following code
+  {            
     Serial.print("Going into autoconnect-mode");
     wifiManager.autoConnect(ViltName);     // This starts the config portal only if it cannot find a previously connected wifi network
     EEPROM.write(0, 1);                    // Changes the 0 byte back to 1, so the next time the ESP boots up it will give the user the 60 seconds to change wifi networks
     EEPROM.commit();                       // Commits the change in the byte
   }
-  else{
+  else
+  {
     Serial.print("Mistakes were made!");   // error message if the byte is not equal to 1 or 2
     Serial.print(EEPROM.read(0));          // prints the byte on the serial monitor
   } 
@@ -53,26 +59,30 @@ void setup() {
    isSetup = false;
 }
  
-void loop() {
-  if(WiFi.status() == WL_CONNECTED){      // Checks if the ESP is connected to a WiFi Network
-    HTTPClient http;                      // Connects to a http Client
-
-       if (scale.is_ready()) {
-    long reading = scale.read();
-    Serial.print("HX711 reading: ");
-    if(isSetup == false)
-    {
-      calibration = reading;
-      isSetup = true;
-    }
-    else
-    {
-      drukWaarde = reading;
-    }
-    
-  } else {
+void loop() 
+{
+  if(WiFi.status() == WL_CONNECTED) // Checks if the ESP is connected to a WiFi Network
+  {      
+    HTTPClient http; // Connects to a http Client
+	
+	if (scale.is_ready()) 
+	{
+		long reading = scale.read();
+		Serial.print("HX711 reading: ");
+		if(isSetup == false)
+		{
+		  calibration = reading;
+		  isSetup = true;
+		}
+		else
+		{
+		  drukWaarde = reading;
+		}
+	} 
+	else 
+	{
     Serial.println("HX711 not found.");
-  }
+	}
     String ServerFinal = Uri + drukWaarde; // Puts the api-address, values from the sensor, and the passwords in one string
     http.begin(Server);     
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
